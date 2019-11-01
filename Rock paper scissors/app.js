91049912ac5eb827f8e255a9fc36689b743019d8
Michaelkonda <michaelkonda11@gmail.com>
2019-10-31 23:15:11 -0500

    Added javascript

--- Rock paper scissors/app.js
+++ Rock paper scissors/app.js
@@ -3,6 +3,20 @@ const game = ()=> {
     let cScore = 0;
 
     const startGame = () =>{
-        
-    }
-}
+      const playBtn = document.querySelector(".intro button");
+      const introscreen = document.querySelector(".intro");
+      const match = document.querySelector(".match");
+      
+      
+      playBtn.addEventListener("click", () => {
+          introscreen.classList.add("fadeOut");
+      });
+    };
+
+    //IS call all the inner function
+    startGame();
+    };
+
+
+    //start the game funtion
+    game();
