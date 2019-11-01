9705b609e51c32059d7f7f52f662a84bdc166edd
Michaelkonda <michaelkonda11@gmail.com>
2019-11-01 00:04:25 -0500

    Added  some functions for play match portion

--- Rock paper scissors/app.js
+++ Rock paper scissors/app.js
@@ -2,6 +2,7 @@ const game = ()=> {
     let pScore = 0;
     let cScore = 0;
 
+    //start the game
     const startGame = () =>{
       const playBtn = document.querySelector(".intro button");
       const introscreen = document.querySelector(".intro");
@@ -10,9 +11,19 @@ const game = ()=> {
       
       playBtn.addEventListener("click", () => {
           introscreen.classList.add("fadeOut");
+          match.classList.add("fadeIn");
       });
     };
+//Play match
+    const playMatch = () => {
+        const options = document.querySelectorAll(".options button");
+        const playerHand = document.querySelector(".player-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        //Computer options
+        const computerOptions = ["rock", "paper", "scissors"];
 
+        const computerNumber = Math.floor(Math.random() * 3);
+    }
     //IS call all the inner function
     startGame();
     };
